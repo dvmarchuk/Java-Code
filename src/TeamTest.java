78ff703bba2f33adea2f9e5cdb61dca9c1e8bb8b
dennismarchuk <dennismarchuk@denniss-mbp.ws.bridgewater.edu>
2016-09-09 14:29:59 -0400

    @DrEricMcGregor here is my change. @dvmarchuk

--- src/TeamTest.java
+++ src/TeamTest.java
@@ -3,7 +3,7 @@ public class TeamTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hi Dennis ");
-		System.out.println("Hello McG, here is my revision");
+		System.out.println("Hello McG, here is my revision ");
 	}
 
 }
