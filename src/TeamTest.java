c037ddbe2ce3403c0d2bb411449330c43edb29ec
dennismarchuk <dennismarchuk@denniss-mbp.ws.bridgewater.edu>
2016-09-09 14:02:32 -0400

    Hi @DrEricMcGregor here is my revision
    @dvmarchuk

--- src/TeamTest.java
+++ src/TeamTest.java
@@ -2,7 +2,8 @@
 public class TeamTest {
 
 	public static void main(String[] args) {
-		System.out.print("Hi Dennis");
+		System.out.println("Hi Dennis ");
+		System.out.println("Hello McG, here is my revision");
 	}
 
 }
