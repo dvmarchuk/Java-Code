c7a608f03c918829b21a8b4964e149f62b1f1a96
dennismarchuk <dennismarchuk@denniss-mbp.ws.bridgewater.edu>
2016-11-04 13:55:09 -0400

    My HuffmanLinkedList and createNodeList

--- HuffmanEncoder/src/HuffmanEncoder.java
+++ HuffmanEncoder/src/HuffmanEncoder.java
@@ -1,6 +1,5 @@
-import java.util.Map;
-import java.util.LinkedList;
-import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -8,7 +7,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 
-@SuppressWarnings("serial")
+@SuppressWarnings({ "unused" })
 public class HuffmanEncoder {
 
 	private static final Boolean DEBUG = true;
@@ -28,8 +27,30 @@ public class HuffmanEncoder {
 
 
 
-	private static HuffmanLinkedList createNodeList(Map<Integer, Integer> map){
-		return null;
+	private static HuffmanLinkedList createNodeList(Map<Integer, Integer> map){//TODO
+		HuffmanLinkedList hll = new HuffmanLinkedList();
+		Set<Entry<Integer, Integer>> set = map.entrySet();
+		
+		Iterator<Entry<Integer, Integer>> setIterator = set.iterator();
+		Entry <Integer, Integer> e = null;
+		HuffmanData data = null;
+		
+		
+		while(setIterator.hasNext()){//while there is a next element in the set
+			e = setIterator.next();
+			data = new HuffmanData(e.getKey(), e.getValue());
+			BinaryNode<HuffmanData> node = new BinaryNode<>(data);
+			hll.add(node);
+			HuffmanData hd;			
+			
+		}
+		
+		if(DEBUG){
+			System.out.println("Huffman sorted Linked LIst: ");
+		}
+		
+		
+		return hll;
 	}
 
 	private static BinaryNode<HuffmanData> createHuffmanTree (HuffmanLinkedList nodeList){
@@ -60,73 +81,73 @@ public class HuffmanEncoder {
 		 * add it to the HashMap and set its frequency to 1, otherwise increment
 		 * the symbols frequency in the HashMap.*/
 
-		
 
-			HashMap<Integer, Integer> hashMap = new HashMap <Integer, Integer>();
 
-			BufferedReader buff = new BufferedReader(new FileReader(file));
+		HashMap<Integer, Integer> hashMap = new HashMap <Integer, Integer>();
 
-			try{
-				
-				int val = 0;
+		BufferedReader buff = new BufferedReader(new FileReader(file));
 
-				while((val = buff.read()) != -1){
-					if(hashMap.containsKey(val)){
-						char vChar = (char)val;
-						System.out.println(vChar);
-					}
-					else{
-						hashMap.put(val, 1);
-					}
+		try{
+			int val = 0;
+
+			while((val = buff.read()) != -1){
+				if(hashMap.containsKey(val)){
+					char vChar = (char)val;
+					System.out.println(vChar);
 				}
+				else{
+					hashMap.put(val, 1);
+				}
+			}
 
-			}catch(Exception e){
+		}catch(Exception e){
 			e.printStackTrace();
-			}
-
-			hashMap.put(EOT_SYM, 1);
-			buff.close();	
-
-			if(DEBUG){
-				System.out.print("Symbol Table: ");
-				for(Integer name : hashMap.keySet()){
-					String key = name.toString();
-					String va = hashMap.get(name).toString();
-					System.out.println(key + " " + va);
-				}
-
-
-
-
-			}
-			return hashMap;
 		}
 
+		hashMap.put(EOT_SYM, 1);
+		buff.close();	
+
+		if(DEBUG){
+			System.out.print("Symbol Table: ");
+			for(Integer name : hashMap.keySet()){
+				String key = name.toString();
+				String va = hashMap.get(name).toString();
+				System.out.println(key + " " + va);
+			}
+
+
+
+
+		}
+		return hashMap;
 	}
 
 
+}
 
 
 
 
-		/*************************** createNodeList() *****************************
-		 * Create a List of BinaryNodes where each Node contains a symbol and a
-		 * frequency field. For each (symbol, frequency) pair in the HashMap add a
-		 * new BinaryNode to the List while keeping the list sorted in increasing order
-		 * according to frequency. **************************************************************************/ /************************** createHuffmanTree() ***************************
-		 * While the List has more than 1 BinaryNode, create a new BinaryNode
-		 * with frequency equal to the sum of the frequencies of the top two Nodes in
-		 * the list. Set the left child equal to the top Node and remove the top Node from * the list. Then set the right child equal to the new top Node and remove the top * Node from the list. Insert the new Node into the sorted List while
-		 * maintaining its sortedness. ***************************************************************************/ /************************ createHuffmanCodeMap() ************************** * Traverse the Huffman Tree using a Depth First Search while maintaining a
-		 * String that represents the path from the root. If descending a left branch
-		 * concatenate a '0' to the string, if descending a right branch concatenate
-		 * a '1' to the string, and if ascending remove the last character. When you
-		 * visit a leaf, add (symbol,code) to the HashMap where symbol is the
-		 * symbol stored in the leaf Node and code is the String that was constructed
-		 * while traversing the Tree. ****************************************************************************/ /***************** writeSerializedHuffmanCodeToFile() *********************
-		 * Write the Huffman code HashMap as a serialized object to an output file ****************************************************************************/ /**************************** encodeFile() *********************************
-		 * While reading each symbol of the input file, output (in binary) the code
-		 * associated with the symbol that is stored the code HashMap. ****************************************************************************/
+
+
+/*************************** createNodeList() *****************************
+ * Create a List of BinaryNodes where each Node contains a symbol and a
+ * frequency field. For each (symbol, frequency) pair in the HashMap add a
+ * new BinaryNode to the List while keeping the list sorted in increasing order
+ * according to frequency. **************************************************************************/ /************************** createHuffmanTree() ***************************
+ * While the List has more than 1 BinaryNode, create a new BinaryNode
+ * with frequency equal to the sum of the frequencies of the top two Nodes in
+ * the list. Set the left child equal to the top Node and remove the top Node from * the list. Then set the right child equal to the new top Node and remove the top * Node from the list. Insert the new Node into the sorted List while
+ * maintaining its sortedness. ***************************************************************************/ /************************ createHuffmanCodeMap() ************************** * Traverse the Huffman Tree using a Depth First Search while maintaining a
+ * String that represents the path from the root. If descending a left branch
+ * concatenate a '0' to the string, if descending a right branch concatenate
+ * a '1' to the string, and if ascending remove the last character. When you
+ * visit a leaf, add (symbol,code) to the HashMap where symbol is the
+ * symbol stored in the leaf Node and code is the String that was constructed
+ * while traversing the Tree. ****************************************************************************/ /***************** writeSerializedHuffmanCodeToFile() *********************
+ * Write the Huffman code HashMap as a serialized object to an output file ****************************************************************************/ /**************************** encodeFile() *********************************
+ * While reading each symbol of the input file, output (in binary) the code
+ * associated with the symbol that is stored the code HashMap. ****************************************************************************/
 
 
 
--- HuffmanEncoder/src/HuffmanLinkedList.java
+++ HuffmanEncoder/src/HuffmanLinkedList.java
@@ -1,9 +1,51 @@
+import java.util.Iterator;
 import java.util.LinkedList;
-@SuppressWarnings("serial")
+import java.util.Map.Entry;
+@SuppressWarnings({ "serial", "unused" })
 
 public class HuffmanLinkedList extends LinkedList <BinaryNode <HuffmanData>>{
-	
+
 	public HuffmanLinkedList(){
 		super();
 	} 
+
+	@SuppressWarnings({ "unchecked" })
+	public boolean add(HuffmanData data){//that takes huffmanData objects and inserts them in order according to frequency
+
+		if(super.size() == 0){
+			BinaryNode<HuffmanData> binN = new BinaryNode<HuffmanData>(data);
+			super.add(binN);
+			return true;
+		}
+
+		Iterator<BinaryNode <HuffmanData>> iterator = super.iterator();//initilizae an iterator to iterate through the linkedlist
+		BinaryNode<HuffmanData> nextData = iterator.next();
+
+
+		while(iterator.hasNext() == true){
+			BinaryNode<HuffmanData> currentNode = iterator.next();//set the current node equal to the iterators next element
+			//TODO
+			if (currentNode.getData().getFrequency() > data.getFrequency()){				//if the current node's frequency is greater than the data's frequency
+				int getIndex = super.indexOf(currentNode);					//1. get the index of the current node in the linked list
+				BinaryNode<HuffmanData> nBinaryN = currentNode;					//2. Create a new BinaryNode to hold the data
+				super.add(getIndex, nBinaryN);					//3. Add the new BinaryNode at the index of the currentNode
+				return true;
+			}
+
+
+		}
+
+		//if the data's frequency is greater than all nodes in the ilnkedList 
+		@SuppressWarnings("rawtypes")
+		BinaryNode binN = new BinaryNode(data);
+		super.add(binN);
+		//add a new BinaryNode with the data to the end of the list
+
+
+		return true;
+
+	}
+
+
+
 }
